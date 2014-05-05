3539b6675237d5fcab29233f724f1980f07162e1
sergyp <sergyp@gmail.com>
2014-05-05 19:38:24 +0100

    Thrift Pojo Generator Testing - Adding BridgeTest

--- thrift-pojo-generator-maven-plugin/src/test/java/thrift/pojo/ThriftPojoGeneratorTest.java
+++ thrift-pojo-generator-maven-plugin/src/test/java/thrift/pojo/ThriftPojoGeneratorTest.java
@@ -9,6 +9,13 @@ import java.io.IOException;
 import java.util.*;
 
 import org.apache.commons.io.FileUtils;
+import thrift.pojo.test.pojo.package1.TestTypesStructPojo;
+import thrift.pojo.test.pojo.package1.TestTypesStructPojoBridge;
+import thrift.pojo.test.testdata.included.package1.EnumStruct;
+import thrift.pojo.test.testdata.included.package1.ObjectInnerStruct;
+import thrift.pojo.test.testdata.included.package1.SimpleStruct;
+import thrift.pojo.test.testdata.included.package1.TestTypesStruct;
+import thrift.pojo.test.testdata.included.package2.SimpleStructPackage2;
 
 import static org.mockito.Mockito.*;
 
@@ -50,7 +57,7 @@ public class ThriftPojoGeneratorTest {
         Assert.assertEquals(18, thriftPojoGenerator.fileToContentMap.size());
 
         String[] a = {"java"};
-        String expectedFilesPath = "src/test/resources/expectedFiles";
+        String expectedFilesPath = "src/test/expectedFiles";
         Iterator<File> expectedFiles = FileUtils.iterateFiles(new File(expectedFilesPath), a, true);
         while (expectedFiles.hasNext()) {
             File expectedFile = expectedFiles.next();
@@ -67,39 +74,36 @@ public class ThriftPojoGeneratorTest {
         protected void writeClass(String folder, String fileName, String content) throws IOException {
             fileToContentMap.put(folder + "/" + fileName, content);
         }
-
     }
 
     @Test
     public void testProducedFilesAsExpected() throws Exception {
+        SimpleStruct simpleStruct = new SimpleStruct("string1");
+        SimpleStructPackage2 simpleStructPackage2 = new SimpleStructPackage2("string2");
+        ObjectInnerStruct objectInnerStruct = new ObjectInnerStruct("string3", simpleStruct, simpleStructPackage2);
+        TestTypesStruct typeStruct = new TestTypesStruct("string4", null, (short) 1, 100, 1000l, true, (byte) 1, 300d, EnumStruct.VAL1, simpleStruct, objectInnerStruct);
 
+        // Conversion should return the same value than provided
+        TestTypesStructPojo testTypesStructPojo = TestTypesStructPojoBridge.getPojoRepresentation(typeStruct);
+        assertProducedPojo(typeStruct, testTypesStructPojo);
+        assertProducedPojo(TestTypesStructPojoBridge.getRemoteRepresentation(testTypesStructPojo), testTypesStructPojo);
+        // TODO: Test Maps
+        // TODO: Get the Collection functionality up, running and tested
     }
-//    @Test
-//    public void testProducedFilesAsExpected() throws Exception {
-////        ThriftPojoGeneratorMock thriftPojoGenerator = new ThriftPojoGeneratorMock();
-//        ThriftPojoGenerator thriftPojoGenerator = new ThriftPojoGenerator();
-//
-//        MavenProject mavenProjectMock = mock(MavenProject.class);
-//        thriftPojoGenerator.setProject(mavenProjectMock);
-////        File outputFile = mock(File.class);
-//        thriftPojoGenerator.setOutputDirectory(new File("/Users/sergio/git/thrift-pojo-generator-maven-plugin/thrift-pojo-generator-maven-plugin/src/test/resources/expectedFiles"));
-//        thriftPojoGenerator.setOutputPostfix("Pojo");
-//        thriftPojoGenerator.setInterfaceName("java.io.Serializable");
-//        thriftPojoGenerator.setDestinationPackage("thrift.pojo.test.pojo");
-//
-//        List<String> sourceList = new ArrayList<String>();
-//        sourceList.add("src/test/resources/thrift-java");
-//        thriftPojoGenerator.setSources(sourceList);
-//
-//        List<String> packageList = new ArrayList<String>();
-//        packageList.add("thrift.pojo.test.testdata.included");
-//        thriftPojoGenerator.setPackageBaseList(packageList);
-//
-//        // Stub testing methods
-//        doNothing().when(mavenProjectMock).addCompileSourceRoot(anyString());
-//
-//        // Execute and verify
-//        thriftPojoGenerator.execute();
-////        verify(thriftPojoGenerator).writeClass("","","");
-//    }
+
+    private void assertProducedPojo(TestTypesStruct typeStruct, TestTypesStructPojo testTypesStructPojo) {
+        Assert.assertEquals(typeStruct.getSimpleString(), testTypesStructPojo.getSimpleString());
+        Assert.assertNull(testTypesStructPojo.getNonRequiredString());
+        Assert.assertEquals(typeStruct.getNumber16b(), testTypesStructPojo.getNumber16b());
+        Assert.assertEquals(typeStruct.getNumber32b(), testTypesStructPojo.getNumber32b());
+        Assert.assertEquals(typeStruct.getNumber64b(), testTypesStructPojo.getNumber64b());
+        Assert.assertEquals(typeStruct.isBooleanValue(), testTypesStructPojo.getBooleanValue());
+        Assert.assertEquals(typeStruct.getByteValue(), testTypesStructPojo.getByteValue());
+        Assert.assertEquals(typeStruct.getDoubleValue(), testTypesStructPojo.getDoubleValue());
+        Assert.assertEquals(typeStruct.getEnumType().name(), testTypesStructPojo.getEnumType().name());
+        Assert.assertEquals(typeStruct.getRef().getName(), testTypesStructPojo.getRef().getName());
+        Assert.assertEquals(typeStruct.getComposedStruct().getStringVariable(), testTypesStructPojo.getComposedStruct().getStringVariable());
+        Assert.assertEquals(typeStruct.getComposedStruct().getInnerStruct().getName(), testTypesStructPojo.getComposedStruct().getInnerStruct().getName());
+        Assert.assertEquals(typeStruct.getComposedStruct().getInnerStructPackage2().getName(), testTypesStructPojo.getComposedStruct().getInnerStructPackage2().getName());
+    }
 }
